0c12ce17725751dd22698a0c8d7e26641e09b06d
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-05 01:26:09 +0200

    Component/TaskBar: Changed the order of static and dynamic items

--- frontend/src/components/TaskBar/index.js
+++ frontend/src/components/TaskBar/index.js
@@ -5,12 +5,13 @@ import './TaskBar.css';
 const TabBar = ({tasks}) => {
 	return (
 		<>
+			<NavLink to="/tasks/add-task" className={({isActive}) => (isActive ? "tab-bar active" : "tab-bar")}>Add Task</NavLink>
+
 			{tasks && tasks.map((task) => {
 				return (
 					<NavLink key={task.id} to={`/tasks/task-${task.id}`} className={({isActive}) => (isActive ? "tab-bar active" : "tab-bar")}>{task.name}</NavLink>
 				)
 			})}
-			<NavLink to="/tasks/add-task" className={({isActive}) => (isActive ? "tab-bar active" : "tab-bar")}>Add Task</NavLink>
 		</>
 	);
 }

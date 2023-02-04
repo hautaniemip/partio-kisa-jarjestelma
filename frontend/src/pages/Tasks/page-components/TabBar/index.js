68101120301994a9b1e10e8f92ffbd041af1d4f9
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-04 23:35:35 +0200

    Page/Tasks: Added TaskBar component

---
+++ frontend/src/pages/Tasks/page-components/TabBar/index.js
@@ -0,0 +1,18 @@
+import { NavLink, useRouterMatch } from 'react-router-dom';
+
+import './TabBar.css';
+
+const TabBar = ({tasks}) => {
+	return (
+		<>
+			{tasks && tasks.map((task) => {
+				return (
+					<NavLink key={task.id} to={`tasks/task-${task.id}`} className={({isActive}) => (isActive ? "tab-bar active" : "tab-bar")}>{task.name}</NavLink>
+				)
+			})}
+			<NavLink to="/tasks/add-task" className={({isActive}) => (isActive ? "tab-bar active" : "tab-bar")}>Add Task</NavLink>
+		</>
+	);
+}
+
+export default TabBar;

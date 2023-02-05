494ec14e633360060a632c713ae7216a7b48e3e2
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-05 19:08:46 +0200

    Server: Added POST route to /api/tasks for managing tasks

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const connection = mysql.createConnection({
 	host: "192.168.200.204",
 	user: "kisa-db",
@@ -30,6 +33,35 @@ app.get("/api/tasks", (req, res) => {
 		}
 		res.json(rows);
 	});
+});
+
+app.post("/api/tasks", (req, res) => {
+	console.log(req.body);
+	connection.query("SET FOREIGN_KEY_CHECKS=0;")
+	connection.query("DELETE FROM Tasks;", (err, rows, fields) => {
+		if (err) {
+			console.log(err)
+			res.status(500);
+		}
+	});
+	connection.query("SET FOREIGN_KEY_CHECKS=1;")
+
+	let newRows = [];
+	for (const task of req.body) {
+		console.log(task)
+		newRows.push([task.id, task.name]);
+	}
+
+	console.log(req.headers);
+
+	connection.query(`INSERT INTO Tasks(id, name) VALUES ?;`, [newRows], (err, rows, fields) => {
+		if (err) {
+			console.log(err)
+			res.status(500);
+		}
+	});
+
+	res.send();
 });
 
 app.get("/api/task/:id", (req, res) => {

63ec59cf7a78c4303547c25743b3330b4c2d155e
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-09 20:11:31 +0200

    Component/Table: Styling changes to fit the table on smaller screens
    Added scroll bar to table-container to stop the whole page from
    scrolling

--- frontend/src/components/Table/index.js
+++ frontend/src/components/Table/index.js
@@ -15,29 +15,31 @@ const Table = ({ columns, data }) => {
 	});
 
 	return (
-		<table {...getTableProps()}>
-			<thead>
-				{headerGroups.map(headerGroup => (
-				<tr {...headerGroup.getHeaderGroupProps()}>
-				{headerGroup.headers.map(column => (
-					<th {...column.getHeaderProps()}>{column.render("Header")}</th>
+		<div className="table-container">
+			<table {...getTableProps()}>
+				<thead>
+					{headerGroups.map(headerGroup => (
+					<tr {...headerGroup.getHeaderGroupProps()}>
+					{headerGroup.headers.map(column => (
+						<th {...column.getHeaderProps()}>{column.render("Header")}</th>
+					))}
+					</tr>
 				))}
-				</tr>
-			))}
-			</thead>
-			<tbody {...getTableBodyProps()}>
-				{rows.map((row, i) => {
-					prepareRow(row);
-					return (
-						<tr {...row.getRowProps()}>
-							{row.cells.map(cell => {
-								return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-							})}
-						</tr>
-					);
-				})}
-			</tbody>
-		</table>
+				</thead>
+				<tbody {...getTableBodyProps()}>
+					{rows.map((row, i) => {
+						prepareRow(row);
+						return (
+							<tr {...row.getRowProps()}>
+								{row.cells.map(cell => {
+									return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+								})}
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
 	);
 }
 

0bf75012fb905ef3d9df92cd81e723b317ae5158
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-09 14:22:49 +0200

    Component/ResultTable: Made new component ResultTable

---
+++ frontend/src/components/ResultTable/index.js
@@ -0,0 +1,89 @@
+import { useState, useEffect, useMemo } from 'react';
+
+import Table from '../Table';
+import LoadingSpinner from '../LoadingSpinner';
+
+const ResultsTable = () => {
+	const [tasks, setTasks] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	const [results, setResults] = useState(null);
+
+	useEffect(() => {
+		let getTasks = () => {
+			fetch("/api/tasks")
+				.then((res) => {
+					if (!res.ok)
+						throw new Error(`HTTP error: ${res.status}`);
+					return res.json();
+				})
+				.then((data) => {
+					setTasks(data);
+					setError(null);
+				})
+				.catch((err) => {
+					setTasks(null);
+					setError(err.message);
+				})
+				.finally(() => {
+					setLoading(false);
+				});
+		};
+
+		getTasks()
+	}, []);
+
+	useEffect(() => {
+		let getResults = () => {
+			fetch("/api/results")
+				.then((res) => {
+					if (!res.ok)
+						throw new Error(`HTTP error: ${res.status}`);
+					return res.json();
+				})
+				.then((data) => {
+					setResults(data);
+					setError(null);
+				})
+				.catch((err) => {
+					setResults(null);
+					setError(err.message);
+				})
+				.finally(() => {
+					setLoading(false);
+				});
+		};
+
+		getResults()
+	}, []);
+
+	const columns = useMemo(
+		() => {
+		if (!tasks)
+			return [];
+
+		let columns = [{ Header: "Joukkue", accessor: "team.name"}]
+		tasks.forEach((task) => {
+			let name = task.name;
+			let id = "team." + task.id.toString();
+			columns.push({ Header: name, accessor: id });
+		});
+
+		columns.push({Header: "Total", accessor: "team.total"})
+
+		return columns;
+		},
+		[tasks]
+	);
+
+	return (
+		<div>
+			{error && (<span>{error}</span>)}
+			{loading && (<LoadingSpinner />)}
+			{tasks && results && <Table columns={columns} data={results} />}
+		</div>
+	);
+}
+
+export default ResultsTable;

42dc55018a2918e95771c1909d8ddca3ee54e216
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-10 21:24:39 +0200

    Component/TeamInfoTable: Refactored the code to its own component

---
+++ frontend/src/components/TeamInfoTable/index.js
@@ -0,0 +1,60 @@
+import { useState, useEffect, useMemo } from 'react';
+
+import Table from '../../components/Table';
+import LoadingSpinner from '../../components/LoadingSpinner';
+
+const TeamInfoTable = () => {
+	const [teams, setTeams] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		let getTeams = () => {
+			fetch("/api/teams")
+				.then((res) => {
+					if (!res.ok)
+						throw new Error(`HTTP error: ${res.status}`);
+					return res.json();
+				})
+				.then((data) => {
+					setTeams(data);
+					setError(null);
+				})
+				.catch((err) => {
+					setTeams(null);
+					setError(err.message);
+				})
+				.finally(() => {
+					setLoading(false);
+				});
+		};
+
+		getTeams()
+	}, []);
+
+	const columns = useMemo(() =>
+		[{
+			Header: "ID",
+			accessor: "id"
+		},
+		{
+			Header: "Joukkue",
+			accessor: "name"
+		},
+		{
+			Header: "Rasti",
+			accessor: "TaskId"
+		}],
+		[]
+	);
+
+	return (
+		<>
+			{error && (<span>{error}</span>)}
+			{loading && (<LoadingSpinner />)}
+			{teams && <Table columns={columns} data={teams} />}
+		</>
+	);
+}
+
+export default TeamInfoTable;

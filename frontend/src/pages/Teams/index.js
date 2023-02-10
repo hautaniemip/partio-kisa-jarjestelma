511b110fa30e8be9f5c7f659919e3aed0d723ef0
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-10 21:25:28 +0200

    Page/Teams: Refactored to use the new TeamInfoTable

--- frontend/src/pages/Teams/index.js
+++ frontend/src/pages/Teams/index.js
@@ -1,59 +1,11 @@
-import React, { useState, useEffect, useMemo } from 'react';
-
-import Table from '../../components/Table';
-import LoadingSpinner from '../../components/LoadingSpinner';
+import TeamInfoTable from '../../components/TeamInfoTable';
 import ManagerForm from '../../components/ManagerForm';
 
 const Teams = () => {
-	const [teams, setTeams] = useState(null);
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(null);
-
-	useEffect(() => {
-		let getTeams = () => {
-			fetch("/api/teams")
-				.then((res) => {
-					if (!res.ok)
-						throw new Error(`HTTP error: ${res.status}`);
-					return res.json();
-				})
-				.then((data) => {
-					setTeams(data);
-					setError(null);
-				})
-				.catch((err) => {
-					setTeams(null);
-					setError(err.message);
-				})
-				.finally(() => {
-					setLoading(false);
-				});
-		};
-
-		getTeams()
-	}, []);
-
-	const columns = useMemo(() =>
-		[{
-			Header: "ID",
-			accessor: "id"
-		},
-		{
-			Header: "Joukkue",
-			accessor: "name"
-		},
-		{
-			Header: "Rasti",
-			accessor: "TaskId"
-		}]
-	);
-
 	return (
 		<div>
 			<h2>Joukkueet</h2>
-			{error && (<span>{error}</span>)}
-			{loading && (<LoadingSpinner />)}
-			{teams && <Table columns={columns} data={teams} />}
+			<TeamInfoTable />
 			<ManagerForm header="Hallitse joukkueita" apiPath="/api/teams" />
 		</div>
 	);

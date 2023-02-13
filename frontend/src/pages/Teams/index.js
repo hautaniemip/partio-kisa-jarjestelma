feba746f7cbf97b461daca140643c07ca7028ea1
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-13 13:02:37 +0200

    Page/Teams: Added variable to refresh the TeamInfoTable

--- frontend/src/pages/Teams/index.js
+++ frontend/src/pages/Teams/index.js
@@ -1,12 +1,20 @@
+import { useState, useEffect } from 'react';
+
 import TeamInfoTable from '../../components/TeamInfoTable';
 import ManagerForm from '../../components/ManagerForm';
 
 const Teams = () => {
+	const [refresh, setRefresh] = useState(false);
+
+	useEffect(() => {
+		setRefresh(false);
+	}, [refresh]);
+
 	return (
 		<div>
 			<h2>Joukkueet</h2>
-			<TeamInfoTable />
-			<ManagerForm header="Hallitse joukkueita" apiPath="/api/teams" />
+			<TeamInfoTable refresh={refresh} />
+			<ManagerForm header="Hallitse joukkueita" apiPath="/api/teams" refresh={setRefresh} />
 		</div>
 	);
 };

d777215b21ebe8f00b1bd2e339fa4b4d6d42b140
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-14 09:44:23 +0200

    App: Added AlertContainer to display alerts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Alert, { AlertContainer } from './components/Alert';
 
 import Home from './pages/Home';
 import Teams from './pages/Teams';
@@ -9,6 +11,14 @@ import Tasks from './pages/Tasks';
 import './App.css';
 
 function App() {
+	const [alerts, setAlerts] = useState([{type: "error", message: "Test"}, {type: "success", message: "Test"}]);
+
+	const closeAlert = (index) => {
+		let data = [...alerts]
+		data.splice(index, 1)
+		setAlerts(data);
+	}
+
 	return (
 		<div className="App">
 			<BrowserRouter>
@@ -16,7 +26,14 @@ function App() {
 					<h1>Kisa palvelu</h1>
 					<Navbar />
 				</header>
+				
 				<div className="main-content">
+					<AlertContainer>
+						{alerts.map((alert, index) => {
+							return <Alert key={index} type={alert.type} message={alert.message} index={index} closeCallback={closeAlert} />
+						})}
+					</AlertContainer>
+
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/teams" element={<Teams />} />

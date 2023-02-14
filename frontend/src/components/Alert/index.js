7caa03096670fc68a00c3ca2e92785bc3db35ead
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-14 09:43:47 +0200

    Component/Alert: Added new component to display alerts

---
+++ frontend/src/components/Alert/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './Alert.css';
+
+const Alert = ({ children, type, message, index, closeCallback }) => {
+	return (
+		<div className={`alert ${type}`}>
+			<span className="close-button" onClick={() => closeCallback(index)} >
+				&times;
+			</span>
+			{children ? children : message}
+		</div>
+	);
+}
+
+const AlertContainer = ({ children }) => {
+	return (
+		<div className="alert-container">
+			{children}
+		</div>
+	);
+}
+
+
+
+export default Alert;
+export { AlertContainer };

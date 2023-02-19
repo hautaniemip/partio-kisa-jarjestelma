47517cbbecf40bc6d1992f1909f48c7ecdec6e8d
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-02-19 12:50:26 +0200

    Server: Fixed Team and Task managing queries

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const connection = mysql.createPool({
 	user: "kisa-db",
 	password: "kisa-db",
 	database: "kisa",
-	connectionLimit: 4
+	connectionLimit: 4,
+	multipleStatements: true
 });
 
 const PORT = process.env.PORT || 3001;
@@ -41,25 +42,27 @@ app.get("/api/teams", (req, res) => {
 });
 
 app.post("/api/teams", (req, res) => {
-	connection.query("SET FOREIGN_KEY_CHECKS=0;")
-	connection.query("DELETE FROM Teams;", (err, rows, fields) => {
-		if (err) {
-			console.log(err)
-			res.status(500);
-		}
-	});
-	connection.query("SET FOREIGN_KEY_CHECKS=1;")
-
+	//connection.query("SET FOREIGN_KEY_CHECKS=0;")
 	let newRows = [];
 	for (const team of req.body)
 		newRows.push([team.id, team.name, 1]);
-	
-	connection.query(`INSERT INTO Teams(id, name, TaskId) VALUES ?;`, [newRows], (err, rows, fields) => {
+
+	connection.query("SET FOREIGN_KEY_CHECKS=0; DELETE FROM Teams; SET FOREIGN_KEY_CHECKS=1; INSERT INTO Teams(id, name, TaskId) VALUES ?;", [newRows], (err, rows, fields) => {
 		if (err) {
 			console.log(err)
 			res.status(500);
 		}
 	});
+	//connection.query("SET FOREIGN_KEY_CHECKS=1;")
+
+	
+	
+	// connection.query(`INSERT INTO Teams(id, name, TaskId) VALUES ?;`, [newRows], (err, rows, fields) => {
+	// 	if (err) {
+	// 		console.log(err)
+	// 		res.status(500);
+	// 	}
+	// });
 
 	res.send();
 });
@@ -87,25 +90,25 @@ app.get("/api/tasks", (req, res) => {
 });
 
 app.post("/api/tasks", (req, res) => {
-	connection.query("SET FOREIGN_KEY_CHECKS=0;")
-	connection.query("DELETE FROM Tasks;", (err, rows, fields) => {
-		if (err) {
-			console.log(err)
-			res.status(500);
-		}
-	});
-	connection.query("SET FOREIGN_KEY_CHECKS=1;")
-
+	//connection.query("SET FOREIGN_KEY_CHECKS=0;")
 	let newRows = [];
 	for (const task of req.body)
 		newRows.push([task.id, task.name]);
-	
-	connection.query(`INSERT INTO Tasks(id, name) VALUES ?;`, [newRows], (err, rows, fields) => {
+
+	connection.query("SET FOREIGN_KEY_CHECKS=0; DELETE FROM Tasks; SET FOREIGN_KEY_CHECKS=1; INSERT INTO Tasks(id, name) VALUES ?;", [newRows], (err, rows, fields) => {
 		if (err) {
 			console.log(err)
 			res.status(500);
 		}
 	});
+	//connection.query("SET FOREIGN_KEY_CHECKS=1;")
+	
+	// connection.query(`INSERT INTO Tasks(id, name) VALUES ?;`, [newRows], (err, rows, fields) => {
+	// 	if (err) {
+	// 		console.log(err)
+	// 		res.status(500);
+	// 	}
+	// });
 
 	res.send();
 });

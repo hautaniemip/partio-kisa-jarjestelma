426e737862453ba55b2f81b95ab4e119be8874b6
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2024-01-20 15:16:08 +0200

    Server: Reverted changes from commit 03b7d4d

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import mysql from "mysql";
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
 
 require('dotenv').config()
 
